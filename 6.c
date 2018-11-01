2ded726dfd64ad43d394e8f83dec23ff77bc2db3
이예현 <shszyh@iclooud.com>
2018-11-01 15:50:51 +0900

    6arr

---
+++ 6.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ROWS	3
+#define COLS	3
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
+	{
+	int i, j;
+	for (i=0; i<ROWS; i++){
+		for (j=0; j<COLS; j++){
+			C[i][j]= A[i][j]+ B[i][j];
+		}
+	}
+}
+void printMatrix(int A[][COLS])
+	{
+	int i, j;
+	
+	for (i=0; i<ROWS; i++){
+		for (j=0; j<COLS; j++){
+			printf("%d	",A[i][j]);
+		}
+		printf("\n");
+	}
+}
+	int A[ROWS][COLS]={//더하기 대상 행렬
+		{2,3,0},
+		{8,9,1},
+		{7,0,5}};
+	
+	int B[ROWS][COLS]={//더하기 대상 행렬
+		{1,0,0},
+		{0,1,0},
+		{0,0,1}};
+	int C[ROWS][COLS];	//덧셈 결과 행렬
+	
+	addMatrix(A,B,C);
+	printMatrix(C);
+	 
+
+	return 0;
+}
