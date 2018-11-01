d8e5288db497b2fcd15251bc7eaf825d1a392c0e
이예현 <shszyh@iclooud.com>
2018-11-01 16:23:15 +0900

    6_2

--- 6.c
+++ 6.c
@@ -1,45 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define ROWS	3
-#define COLS	3
+#define SIZE 5
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+ int nums[SIZE];
+ int i;
 
-void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
-	{
-	int i, j;
-	for (i=0; i<ROWS; i++){
-		for (j=0; j<COLS; j++){
-			C[i][j]= A[i][j]+ B[i][j];
-		}
-	}
+printf("input %i  number:", SIZE);
+
+for (i=0; i<SIZE; i++)
+{
+	scanf("%d",&nums[i]);
 }
-void printMatrix(int A[][COLS])
-	{
-	int i, j;
+
 	
-	for (i=0; i<ROWS; i++){
-		for (j=0; j<COLS; j++){
-			printf("%d	",A[i][j]);
-		}
-		printf("\n");
-	}
+printf("sorted numbers are:");
+for (i=0;i<SIZE;i++)
+{
+	printf("%d", nums[i]);
 }
-	int A[ROWS][COLS]={//더하기 대상 행렬
-		{2,3,0},
-		{8,9,1},
-		{7,0,5}};
 	
-	int B[ROWS][COLS]={//더하기 대상 행렬
-		{1,0,0},
-		{0,1,0},
-		{0,0,1}};
-	int C[ROWS][COLS];	//덧셈 결과 행렬
-	
-	addMatrix(A,B,C);
-	printMatrix(C);
-	 
 
 	return 0;
 }
